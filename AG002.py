990bd8917f38aad34e064b19792d39e48cb72dc0
Jéssica Guerzoni <82246276+jessguerzoni@users.noreply.github.com>
2026-09-19 21:16:10 -0300

    Update AG002.py

--- AG002.py
+++ AG002.py
@@ -1,4 +1,29 @@
-import pandas as np
-import numpy as np
+import pandas as pd
 
-DT=pd.
+#01 - 02
+#fazer leitura dos dados
+
+df=pd.read_csv('Wholescale costumers.csv',delimiter=',')
+
+
+#03
+
+#Converter os valores para numeros inteiros
+
+#convertendo os dados
+df = df.replace({"Channel": {"HoReCa": 0, "Retail": 1},
+"Region": {"Lisbon": 0, "Oporto": 1, "Other": 2},
+})
+
+df[["Channel", "Region"]] = df[["Channel", "Region"]].astype("int64")
+
+print(df[["Channel", "Region"]].dtypes)
+print(df["Channel"].value_counts())
+print(df["Region"].value_counts())
+
+#04 - Reordenar as colunas
+
+new_order = ["Region", "Fresh", "Milk", "Grocery", "Frozen","Detergents_Paper", "Delicatessen", "Channel"]
+
+df = df.reindex(columns=new_order)
+print(list(df.columns)) 
